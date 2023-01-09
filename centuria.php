64df1c4ce943c2aaea25202d29223ac348499942
jamoran1356 <jamoran1356@gmail.com>
2023-01-09 13:07:24 -0400

    Update

---
+++ centuria.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+*/
+function century($year){
+$r = $year/100;
+$d = sprintf("%.2f", $r);
+$d = explode(".", $d);
+    if ($d[1]>0){
+        $century = $d[0]+1;
+        echo $century;
+    } else{
+        $century = $d[0];
+        echo $century;
+    }
+}
+?>
